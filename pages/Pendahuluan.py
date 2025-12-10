f9f045188f2fd910c5e6220b870443ad3d78ceec
ayesha syahda ardiningrum <ayeshasyahda5@gmail.com>
2025-12-10 21:38:14 +0700

    mencoba lagi

--- pages/Pendahuluan.py
+++ pages/Pendahuluan.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
-st.title("Welcome to the kelompok 4's Home Page")
-st.write("maaf yaa belum ada isinya :(")
+st.title("Dataset Saham Indonesia✦")
+st.write("##### Anggota Kelompok ฅ^>⩊<^ฅ =")
+st.write("- Ayesha Syahda Ardiningrum")
+st.write("- Fitrianisa Nabila")
+st.write("- Helmi Sulfa Yulidiawati")
+st.write("- Ade Febriani")
+
+st.write("### Pendahuluan")
+
+st.markdown(
+    """
+    Pasar modal Indonesia memiliki peran yang sangat penting dalam mendukung pertumbuhan ekonomi nasional. Indeks Harga Saham Gabungan (IHSG) menjadi indikator utama yang mencerminkan kinerja pasar saham Indonesia secara keseluruhan. Dengan semakin meningkatnya minat terhadap analisis pasar, kebutuhan akan data historis yang lengkap dan dapat diolah menjadi sangat penting, baik bagi investor, peneliti, maupun akademisi. Dataset Indonesia Stock Dataset (IHSGStockData) yang disediakan oleh pengguna Kaggle muamkh menjadi salah satu sumber data terbuka yang menyediakan rekaman harga saham berbagai emiten yang tergabung dalam IHSG. Dataset ini memberi kesempatan luas bagi pengguna untuk melakukan analisis tren, pengukuran volatilitas, analisis risiko, dan bahkan pemodelan prediktif menggunakan pendekatan statistik maupun machine learning. Oleh karena itu, penelitian berbasis dataset ini dapat memberikan gambaran mengenai pola pergerakan harga saham dan karakteristik pasar modal Indonesia.
+    """
+)
