b0645fdef26aaefa3f8e5f677b09af62f791a34b
sulfa <Ambsulfa@gmail.com>
2025-12-10 22:08:26 +0700

    menambah kesimpulan

---
+++ pages/Kesimpulan.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+st.write("### kesimpulan")
+
+st.markdown(
+    """
+    Dataset IHSGStockData dari Kaggle merupakan sumber data yang kaya dan fleksibel untuk berbagai jenis analisis pasar modal. Dengan struktur data yang lengkap dan frekuensi waktu yang beragam, dataset ini dapat digunakan untuk memahami tren pergerakan harga saham, mengukur volatilitas, membangun model risiko, hingga memprediksi harga menggunakan metode statistika maupun machine learning. Visualisasi dan analisis dasar menunjukkan bahwa pasar Indonesia memiliki dinamika yang kompleks dengan karakteristik volatilitas yang dapat berubah-ubah mengikuti kondisi ekonomi. Korelasi antar saham juga memberikan wawasan penting bagi investor yang ingin membangun portofolio yang efisien. Secara keseluruhan, dataset ini sangat bermanfaat untuk penelitian akademik, studi pasar, serta eksperimen terhadap strategi investasi.
+    """
+)
+
+st.write("###  Referensi / Daftar Pustaka")
+
+st.markdown(
+    """
+    Dataset Saham Indonesia (IHSGStockData) — Kaggle, oleh muamkh.
+    """
+)
+st.markdown(
+    """
+    Investopedia — Moving Average, Technical Analysis, dan definisi Volatility.
+    """
+)
+st.markdown(
+    """
+   Makalah dan artikel terkait penggunaan LSTM untuk prediksi harga saham Indonesia.
+    """
+)
+st.markdown(
+    """
+  Sumber data harga saham publik seperti Yahoo Finance (sebagai rujukan teknis format data).
+    """
+)
+
+
+
+
+
+
+
