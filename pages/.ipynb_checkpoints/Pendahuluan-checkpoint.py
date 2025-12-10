39833420bab3b8fa1cd43d20404f52e7f5dc55cc
Fitriannisaa Nabila <nabila.fitri494@gmail.com>
2025-12-11 02:03:33 +0700

    save local changes

---
+++ pages/.ipynb_checkpoints/Pendahuluan-checkpoint.py
@@ -0,0 +1,57 @@
+import streamlit as st
+
+st.title("Dataset Saham Indonesia✦")
+st.write("##### Anggota Kelompok ฅ^>⩊<^ฅ =")
+st.write("- Ayesha Syahda Ardiningrum")
+st.write("- Fitriannisaa Nabila")
+st.write("- Helmi Sulfa Yulidiawati")
+st.write("- Ade Febriani")
+
+st.write("### Pendahuluan")
+
+st.markdown(
+    """
+    Analisis data ekonomi merupakan langkah penting untuk memahami bagaimana indikator makroekonomi berperan dalam menggambarkan kondisi suatu negara. Dalam konteks Indonesia, tiga indikator utama yang sangat sering digunakan adalah Produk Domestik Bruto (PDB), Inflasi, dan Indeks Harga Saham Gabungan (IHSG).
+
+- PDB menunjukkan total nilai produksi barang dan jasa dalam sebuah negara.
+
+- Inflasi mencerminkan tingkat perubahan harga secara umum.
+
+- IHSG menggambarkan kinerja pasar saham Indonesia secara keseluruhan.
+
+Ketiga indikator ini saling berkaitan dan bersama-sama dapat memberikan gambaran mengenai stabilitas serta pertumbuhan ekonomi. Oleh karena itu, penelitian ini bertujuan untuk melakukan visualisasi serta analisis distribusi masing-masing variabel sehingga dapat diketahui pola dan karakteristiknya.
+
+    """
+)
+
+st.write("### Deskripsi Data")
+
+st.markdown(
+    """
+    Dataset yang dianalisis terdiri dari 300 baris data dengan empat variabel utama:
+    """
+)
+st.markdown(
+    """
+   - Tahun (2010–2024)
+Data tahun tidak berurutan karena diambil secara acak, tetapi tetap berada dalam rentang 2010 sampai 2024. Variabel ini berfungsi sebagai konteks waktu terhadap data ekonomi.
+    """
+)
+st.markdown(
+    """
+   - PDB
+Nilai PDB dalam dataset berada di sekitar kisaran 10.000–14.000, menunjukkan variasi ekonomi yang cukup luas. Nilai ini merupakan angka simulasi yang menyerupai pola pertumbuhan ekonomi Indonesia.
+    """
+)
+st.markdown(
+    """
+  - Inflasi (%)
+Inflasi berada pada kisaran 2% – 5%, mencerminkan kondisi stabil dan realistis sebagaimana rata-rata inflasi Indonesia dalam 10 tahun terakhir.
+    """
+)
+st.markdown(
+    """
+ - IHSG
+Nilai IHSG berada di rentang 6.000–7.500, yang merepresentasikan pergerakan umum pasar saham Indonesia.
+    """
+)
