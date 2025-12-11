a7c418a32fbe5e01a7a580e7daa4440fd3f1d117
ayesha syahda ardiningrum <ayeshasyahda5@gmail.com>
2025-12-11 12:16:47 +0700

    menghapus file

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -5,7 +5,6 @@ pages = [
     st.Page(page="pages/Visualisasi.py", title="Visualisasi Data┈•✦", icon="🌸"),
     st.Page(page="pages/Analisis.py", title="Analisis┈•✦", icon="🌻"),
     st.Page(page="pages/Kesimpulan.py", title="Kesimpulan┈•✦", icon="🎀"),
-    st.Page(page="pages/.Data.py", title="Data┈•✦", icon="📟"),
 ]
 
 pg = st.navigation(
