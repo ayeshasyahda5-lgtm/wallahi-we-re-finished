0a3ebfb9407c24cce62fb77b5382a689946e60dd
ayesha syahda ardiningrum <ayeshasyahda5@gmail.com>
2025-12-18 13:01:05 +0700

    mengubah interpretasi dalam analisis

--- pages/Analisis.py
+++ pages/Analisis.py
@@ -7,7 +7,7 @@ st.write("────────────────────⋆⋅☆�
 
 st.markdown(
     """
-   Menarik benang merah dari ketiga visualisasi di atas, terdapat pola segmentasi pasar yang jelas.
+   Menarik hasil dari ketiga visualisasi yang kami tampilkan, terdapat pola segmentasi pasar yang jelas.
 Pertama, struktur pasar saham Indonesia sangat heterogen (tidak seragam). Dominasi papan "Pengembangan" mengartikan bahwa bursa kita sedang didorong oleh perusahaan-perusahaan skala menengah yang memiliki potensi besar untuk bertumbuh, bukan hanya didominasi oleh raksasa korporasi (Papan Utama).
 Kedua, terdapat hubungan terbalik yang menarik (meski perlu uji korelasi lebih lanjut) antara grafik "Jumlah Lembar Saham" dan "Harga Saham Tertinggi". Emiten yang memiliki harga per lembar sangat tinggi (di grafik ketiga) jarang muncul sebagai emiten dengan jumlah lembar saham terbanyak (di grafik kedua). Sebaliknya, emiten dengan miliaran lembar saham seringkali memiliki harga nominal yang lebih rendah. Ini menunjukkan karakteristik profil risiko dan tipe investor yang berbeda; saham harga tinggi biasanya diburu untuk investasi jangka panjang, sementara saham dengan volume lembar melimpah seringkali menjadi sasaran pedagang harian karena likuiditasnya.
     """
